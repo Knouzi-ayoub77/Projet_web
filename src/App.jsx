e7dc8b633c2b593807726f688dd16df3b93dd3d7
bilalos0jabrinho2005-hue <bilalos0jabrinho2005@gmail.com>
2026-05-06 17:34:42 +0100

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import Home from "./pages/Home";
 import PayingForm from './pages/PayingForm';
 import "./App.css";
 function App() {
-  return <PayingForm/>;
+  return(
+      <PayingForm />
+     
+  );
 }
 
 export default App;
